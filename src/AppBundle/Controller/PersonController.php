852be808dadb174d740f6f2f4b03104c3ad9d17f
CLP <clpaustralia@gmail.com>
2017-02-18 23:35:15 +1100

    Add person search

--- src/AppBundle/Controller/PersonController.php
+++ src/AppBundle/Controller/PersonController.php
@@ -51,4 +51,62 @@ class PersonController extends Controller
 
   }
 
+  /**
+   * @Route("/showPerson/{id}", name="show_person")
+   */
+  public function showPersonAction(Request $request, $id)
+  {
+    
+    $repo = $this->getRepo(); 
+    $person = $repo->findOneById($id);
+    if (!$person) 
+    {
+      throw $this->createNotFoundException("Unable to find person by id: $id");
+    }
+    return $this->render('person_show.html.twig', array('person' => $person));
+
+  }
+
+  /**
+   * @Route("/searchPerson", name="search_person")
+   */
+  public function searchPersonAction(Request $request)
+  {
+    return $this->render('person_search.html.twig'); 
+  }
+
+  /**
+   * @Route("/listPerson", name="list_person")
+   */
+  public function listPersonAction(Request $request){
+
+    $firstName = $request->request->get("firstName");
+    $lastName = $request->request->get("lastName");
+
+    $repo = $this->getRepo();
+    $query = $repo->createQueryBuilder('p')
+      ->where('p.firstName like :firstName')
+      ->andWhere('p.lastName like :lastName')
+      ->setParameter('firstName', $firstName.'%')
+      ->setParameter('lastName', $lastName.'%')
+      ->getQuery();
+    $result = $query->getResult();
+
+    //TODO: search
+    $person = new Person();
+    $person->setTitle('Mr')->setFirstName('Test Person 1 Firstname')->setLastName('Test Person 1 Last Name');
+    $personList[] = $person;
+    $person = new Person();
+    $person->setTitle('Miss')->setFirstName('Test Person 2 Firstname')->setLastName('Test Person 2 Last Name');
+    $personList[] = $person;
+    
+    return $this->render('person_list.html.twig', array('personList' => $result)); 
+
+  }
+
+  private function getRepo()
+  {
+    return $this->getDoctrine()->getRepository('AppBundle:Person');
+  }
+
 }
